c6b98bfa7aab33d0ed51be497e4ec366ff8efd8b
Gerseneck <wang14240@gmail.com>
2023-07-18 20:28:17 -0400

    added links to study activity

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -2,6 +2,7 @@ class StudyController < CardSetsController
   before_action :set_card_set
 
   def show
+    @card_set_id = @card_set.friendly_id
     @cards = @card_set.cards.to_json
     @index = 0
   end
