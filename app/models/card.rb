daae7fa3482304971b9c5357870bc089bb68e218
Gerseneck <wang14240@gmail.com>
2023-10-15 17:47:10 -0400

    added server side validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :card_set
+
+  validates :term, presence: true
+  validates :definition, presence: true
 end
--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -4,4 +4,15 @@ class CardSet < ApplicationRecord
   has_rich_text :description
   has_many :cards, dependent: :destroy, inverse_of: :card_set
   accepts_nested_attributes_for :cards, reject_if: :all_blank, allow_destroy: true
+
+  validates :name, presence: true
+  validates_associated :cards 
+
+  validate :has_required_cards?
+
+  private
+
+  def has_required_cards?
+    errors.add(:minimum, "You must have at least 3 terms in a set.") if cards.length < 3
+  end
 end
