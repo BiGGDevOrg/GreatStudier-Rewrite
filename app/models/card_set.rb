ccb02d7ed9c600b012067f3df1a5ebcdfee3b706
Gerseneck <wang14240@gmail.com>
2023-10-15 23:41:06 -0400

    merge broke stuff again

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -1,5 +1,6 @@
 class CardSet < ApplicationRecord
   include FriendlyId
+  friendly_id :name, use: :slugged
   has_rich_text :description
   has_many :cards, dependent: :destroy, inverse_of: :card_set
   accepts_nested_attributes_for :cards, reject_if: :all_blank, allow_destroy: true
